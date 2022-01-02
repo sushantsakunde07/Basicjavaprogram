9de2e25f97a3be200da7580146ef1ace1f3bdc88
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-02 19:53:34 +0530

    Added: Harmonic number

--- Basicjavaprogram/Javabasicprogram.java
+++ Basicjavaprogram/Javabasicprogram.java
@@ -1,20 +1,24 @@
 package Basicjavaprogram;
 import java.util.*;
 public class Javabasicprogram {
-	public static void main(String[] args)
+	public static void main(String arg[])
 	{
-	int N=0,number=0;
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter Nth number");
-    N=scan.nextInt();
-	scan.close();
-	if (0 <= N && N < 31) {
-	for (int i=0; i<N;i++) {
-		number=(int)Math.pow(2,i);
-		System.out.println(" "+number);	
-	}}
-	else {
-		  System.out.println("Enter number less than 31 and greater than minus 1");
+	double a;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a number");
+	a=sc.nextDouble();
+	sc.close();
+	double h=harmonicnumber(a);
+	System.out.println("Harmonic value = "+h);
 	}
+    static double harmonicnumber(double n) 
+    {
+	double sum=0;
+	for(double  i=1;i<=n;i++)
+	{
+	sum=sum+(1/i);
+	}
+	return sum;
 }
 }
+
