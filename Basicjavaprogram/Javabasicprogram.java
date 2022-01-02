224dda2ffbe1e0b86ea967f7cd76781834cc2fa8
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-02 18:27:03 +0530

    Added: leap year

--- Basicjavaprogram/Javabasicprogram.java
+++ Basicjavaprogram/Javabasicprogram.java
@@ -1,33 +1,16 @@
 package Basicjavaprogram;
 import java.util.*;
 public class Javabasicprogram {
-public static void main(String[] args) {
-	int number;
-	int hcount=0 ,tcount=0;
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter number of times to Flip coin");
-	number=scan.nextInt();
-	scan.close();
-	if (number>0)
+	public static void main(String[] args)
 	{
-    while (hcount+tcount <number)
-    	{
-	    int coin=(int)(Math.floor(Math.random()*2));
-		if (coin ==0)
-		{
-		System.out.println("Heads");
-	     hcount++;
-		}
-		else{ 
-			System.out.println("Tails");
-			tcount++;
-		    }
-	    }
-	}
-	   else{
-		   System.out.println("enter number greater than zero");
-	       }
-	       System.out.println("percentage of heads vs tails is"+(hcount/tcount)*100);
+	int year=0;
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter Year");
+    year=scan.nextInt();
+	scan.close();
+	if(year%4==0 && year%100!=0 || year%400==0)
+	  System.out.println("It is Leap year");
+	else
+	  System.out.println("It is Not a leap year");
 }
 }
-
