5be7e8290274b45c0844d842773a8c7f4136e53c
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-02 20:05:09 +0530

    Added: compute quotient & remainder

--- Basicjavaprogram/Javabasicprogram.java
+++ Basicjavaprogram/Javabasicprogram.java
@@ -1,28 +1,19 @@
 package Basicjavaprogram;
 import java.util.*;
 public class Javabasicprogram {
-	public static void main(String[] args) {
-	      Scanner scan=new Scanner(System.in);
-	      System.out.println("Enter a number: ");
-	      int number = scan.nextInt();
-	      scan.close();
-		  if (number>0)
-		     {
-	      while (number%2==0)
-	          {
-	            System.out.print(2  +" ");
-	            number /= 2;
-	           }
-	      for (int i = 3; i <= Math.sqrt(number); i+= 2)
-	          {
-	      while (number%i == 0)
-	            {
-	              System.out.print(i + " ");
-	              number /= i; 
-	            }
-	        }
-	      if (number > 2)
-	          System.out.println(number);
-}
+public static void main(String[] args) {
+	    int dividend = 0, divisor = 0;
+	    Scanner scan1= new Scanner(System.in);
+	    System.out.println("Enter dividend");
+	    dividend=scan1.nextInt();
+	    scan1.close();
+	    Scanner scan2= new Scanner(System.in);
+	    System.out.println("Enter divisor");
+	    divisor=scan2.nextInt();
+	    scan2.close();
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
 }
 }
