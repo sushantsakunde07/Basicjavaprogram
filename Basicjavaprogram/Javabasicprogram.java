1ecbfe840b50d894940cdc39623c19d5a1030047
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-02 20:09:36 +0530

    Added: Swaping number

--- Basicjavaprogram/Javabasicprogram.java
+++ Basicjavaprogram/Javabasicprogram.java
@@ -2,18 +2,16 @@ package Basicjavaprogram;
 import java.util.*;
 public class Javabasicprogram {
 public static void main(String[] args) {
-	    int dividend = 0, divisor = 0;
-	    Scanner scan1= new Scanner(System.in);
-	    System.out.println("Enter dividend");
-	    dividend=scan1.nextInt();
-	    scan1.close();
-	    Scanner scan2= new Scanner(System.in);
-	    System.out.println("Enter divisor");
-	    divisor=scan2.nextInt();
-	    scan2.close();
-	    int quotient = dividend / divisor;
-	    int remainder = dividend % divisor;
-	    System.out.println("Quotient = " + quotient);
-	    System.out.println("Remainder = " + remainder);
+	 int Number1, Number2, Temp;
+	 Scanner scan = new Scanner(System.in);
+	 System.out.println("Enter First Number");
+	 Number1=scan.nextInt();
+	 System.out.println("Enter second Number");
+	 Number2=scan.nextInt();
+	 scan.close();
+	 Temp=Number1;
+	 Number1=Number2;
+	 Number2=Temp;
+	 System.out.println("Numbers after swapping "+Number1   +Number2); 
 }
 }
