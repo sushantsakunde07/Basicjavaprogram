3d7d261a52b657c3c1a4623a2be171c95cb12795
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-02 20:00:51 +0530

    Added: Factors

--- Basicjavaprogram/Javabasicprogram.java
+++ Basicjavaprogram/Javabasicprogram.java
@@ -1,24 +1,28 @@
 package Basicjavaprogram;
 import java.util.*;
 public class Javabasicprogram {
-	public static void main(String arg[])
-	{
-	double a;
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a number");
-	a=sc.nextDouble();
-	sc.close();
-	double h=harmonicnumber(a);
-	System.out.println("Harmonic value = "+h);
-	}
-    static double harmonicnumber(double n) 
-    {
-	double sum=0;
-	for(double  i=1;i<=n;i++)
-	{
-	sum=sum+(1/i);
-	}
-	return sum;
+	public static void main(String[] args) {
+	      Scanner scan=new Scanner(System.in);
+	      System.out.println("Enter a number: ");
+	      int number = scan.nextInt();
+	      scan.close();
+		  if (number>0)
+		     {
+	      while (number%2==0)
+	          {
+	            System.out.print(2  +" ");
+	            number /= 2;
+	           }
+	      for (int i = 3; i <= Math.sqrt(number); i+= 2)
+	          {
+	      while (number%i == 0)
+	            {
+	              System.out.print(i + " ");
+	              number /= i; 
+	            }
+	        }
+	      if (number > 2)
+	          System.out.println(number);
 }
 }
-
+}
