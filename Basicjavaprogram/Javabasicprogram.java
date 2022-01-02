d745085f1bf7de04945d3d88a5cdccc6b6388381
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-02 20:20:23 +0530

    Added: Vowels in sentence

--- Basicjavaprogram/Javabasicprogram.java
+++ Basicjavaprogram/Javabasicprogram.java
@@ -2,14 +2,15 @@ package Basicjavaprogram;
 import java.util.*;
 public class Javabasicprogram {
 public static void main(String[] args) {
-	    int number;
-	    System.out.println("Enter a number to check if it's odd or even");
-	    Scanner scan = new Scanner(System.in);
-	    number = scan.nextInt();
-	    scan.close();
-	    if (number % 2 == 0)
-	      System.out.println("The number is even.");
-	    else
-	      System.out.println("The number is odd.");
+	Scanner scan =new Scanner (System.in);
+    System.out.println("Enter a sentence");
+    String str =scan.nextLine();
+    scan.close();
+    for(int i=0; i<str.length(); i++) {
+    if(str.charAt(i) == 'a'|| str.charAt(i) == 'e'|| str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'A'||str.charAt(i) == 'E'||str.charAt(i) == 'I'||str.charAt(i) == 'O'||str.charAt(i) == 'U')
+    {
+      System.out.println("Given sentence contains "+str.charAt(i)+" at the index "+i);
+    }
+}
 }
 }
